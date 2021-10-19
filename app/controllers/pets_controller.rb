d926b7309b4c7e21344e8210512f654e1897dcc0
Diana Rybikov <diana.ryb214@gmail.com>
2021-10-18 22:30:59 -0600

    show method

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,4 +3,10 @@ class PetsController < ApplicationController
         @pets = Pet.all 
         render json: @pets
     end
+
+    def show
+        @pet = Pet.find(params[:id])
+        render json: @pet
+    end
+
 end
