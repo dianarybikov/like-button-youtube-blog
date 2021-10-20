e8ab85dcb54e678ec68a1bc42ad73c741e3adcfa
Diana Rybikov <diana.ryb214@gmail.com>
2021-10-20 14:14:27 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pet.destroy_all
+
+Pet.create(
+    name: 'Ted', 
+    img: 'http://farm3.staticflickr.com/2401/1987110561_263a8860b0.jpg', 
+    quote: "Attention!!!!!", 
+    age:"8 months", 
+    likes: 0)
